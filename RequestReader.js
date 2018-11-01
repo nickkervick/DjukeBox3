dcc0b5bcaffd6a88cdd3942cf5714c07cffe8130
nickkervick <43652112+nickkervick@users.noreply.github.com>
2018-11-01 17:10:12 +0000

    Add files via upload

--- RequestReader.js
+++ RequestReader.js
@@ -1,19 +1,29 @@
 
-//
-// Code was learned from the following yt channel
-// https://www.youtube.com/watch?v=nUUPedePJ4o&list=PLGCjwl1RrtcRTbJ9WBL9exbUFNqwzz9if&index=4&fbclid=IwAR2adERVaLnfZFNkATyNI65hsmpTUuH4mhkd6uehQTHq9EdvXOPLrgw44Gc
+// Found this code here: https://www.jackrutorial.com/2018/01/building-web-app-crud-using-firebase.html
+ var tblUsers = document.getElementById("RequestList");
+ var dbRef = firebase.database().ref('Requests/');
+ var rowIndex = 1;
 
-var rootRef = firebase.database().ref().child("Requests");
-//loop for every child in the requests object
-rootRef.on("child_added", snap => {
+//create tabel
+ dbRef.once('value', function(snapshot) {
+   snapshot.forEach(function(childSnapshot) {
+  var childKey = childSnapshot.key;
+  var childData = childSnapshot.val();
 
+  var row = tblUsers.insertRow(rowIndex);
+  var requestId = row.insertCell(0);
+  var trackName = row.insertCell(1);
+  var artistName = row.insertCell(2);
+  var genreType = row.insertCell(3);
+  var trackStatus = row.insertCell(4);
+  var btnQue = row.insertCell(5);
+  requestId.appendChild(document.createTextNode(childKey));
+  trackName.appendChild(document.createTextNode(childData.track_name));
+  artistName.appendChild(document.createTextNode(childData.artist_name));
+  genreType.appendChild(document.createTextNode(childData.genre_type));
+  trackStatus.appendChild(document.createTextNode(childData.track_status));
+  $("#RequestList").append("<input type='button'>");
 
-      var track = snap.child("TrackName").val();
-      var artist = snap.child("Artist").val();
-      var genre = snap.child("Genre").val();
-//      Add output to the table in the html file
-        $("#RequestList").append("<tr><td>" + track + "</td><td>"
-                                  + artist + "</td><td>" + genre +"</td></tr>" );
-
-
-});
+  rowIndex = rowIndex + 1;
+   });
+ });
